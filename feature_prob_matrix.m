2fe7510ae183df26c6f668e01f7c3dbdc15d24af
Soumyadeep Paul <35990855+spaul1995@users.noreply.github.com>
2021-07-19 22:45:06 +0900

    Update feature_prob_matrix.m

--- feature_prob_matrix.m
+++ feature_prob_matrix.m
@@ -1,6 +1,6 @@
 % NanobubbleDigitizer data analysis package licence details:
 % 
-% Copyright Soumyadeep Paul (soumyadeep.paul@thml.t.u-tokyo.ac.jp)
+% Copyright 2009-2014 Soumyadeep Paul (soumyadeep.paul@thml.t.u-tokyo.ac.jp)
 % 
 % This file is part of NanobubbleDigitizer data analysis package.
 % 
@@ -10,7 +10,7 @@
 % nanoscopic bubble dynamics confined within solid-state nanopores. This
 % package is released under the GNU GPL.
 % 
-% The copyright of arb is held by Soumyadeep Paul.
+% The copyright of NanobubbleDigitizer is held by Soumyadeep Paul.
 % 
 % NanobubbleDigitizer is released under the GNU GPL.  NanobubbleDigitizer 
 % is free software: you can redistribute it and/or modify it under the 
